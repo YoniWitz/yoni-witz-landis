c7309799aa7caed578f970f50325ce03e7d725c2
Yoni Hirshowitz <jonhirsh39@gmail.com>
2020-07-31 11:33:12 -0400

    added routes

---
+++ dev-server/api/user/user-routes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/user', (req, res) => {
+    res.send('get.user - get all users');
+});
+
+module.exports = router;
