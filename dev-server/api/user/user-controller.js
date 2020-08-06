26937bb9375a9971293c3cafe6a08eafd086c6f4
Yoni Hirshowitz <jonhirsh39@gmail.com>
2020-08-06 16:14:24 -0400

    fixed tags

--- dev-server/api/user/user-controller.js
+++
@@ -1,3 +0,0 @@
-module.exports = function index(req, res) {
-    return res.status(200).json({ message: 'Hello World' });
-}
--- dev-server/routes.js
+++ dev-server/routes.js
@@ -1,5 +1,4 @@
 const applicantRoutes = require('./api/applicant/applicants-routes');
-const userRoutes = require('./api/user/user-routes');
 const regRoutes = require('./api/register/register-routes');
 const authRoutes = require('./api/auth/auth-routes');
 
@@ -8,5 +7,4 @@ module.exports = function registerRoutes(app) {
     app.use('/api', regRoutes);
     app.use('/api', applicantRoutes);
     app.use('/api', authRoutes);
-    app.use('/api', userRoutes);
 }
